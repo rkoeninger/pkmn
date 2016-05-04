e4fd2ef2a46247b06a932b5be1133aa0407b6217
Robert Koeninger <rkoeninger@att.net>
2016-05-04 18:35:43 -0400

    Added struct member lifetime parameters
    Removed uses of Box
    Added more species and a specimen
    Made strings all &'static lifetime
    Added fmt implementations for Element and StoneType

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,7 @@ enum StoneType {
 }
 
 struct Move {
-	name: String,
+	name: &'static str,
 	type1: Element
 	//ppMax:    u32
 	//accuracy: u32 // 0 - 100
@@ -55,31 +55,83 @@ struct Move {
 	//effectPropibility: u32 // 0 - 100
 }
 
-struct Transform {
+struct Transform<'a> {
 	mode: TransformMode,
-	into: Box<Species>
+	into: &'a Species<'a>
 }
 
-struct Species {
+struct Species<'a> {
 	index: u32,
-	name: String,
+	name: &'static str,
 	type1: Element,
 	type2: Option<Element>,
-	transforms: Vec<Transform>,
+	transforms: Vec<Transform<'a>>,
 	schedule: Vec<(u32, Move)>
 }
 
-impl fmt::Display for Species {
+impl<'a> fmt::Display for Species<'a> {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "#{} {}", self.index, self.name)
+		let _ = writeln!(f, "#{}", self.index);
+		let _ = writeln!(f, "{}", self.name);
+		let _ = write!(f, "{}", self.type1);
+		let _ = match self.type2 {
+			Some(ref t) => writeln!(f, "/{}", t),
+			None => writeln!(f, "")
+		};
+		for trans in self.transforms.iter() {
+			let _ = write!(f, "transforms by ");
+			let _ = match trans.mode {
+				TransformMode::ByLevel(ref x) => write!(f, "level {} ", x),
+				TransformMode::ByStone(ref x) => write!(f, "{} ", x),
+				TransformMode::ByTrade => write!(f, "trade ")
+			};
+			let _ = writeln!(f, "into {}", trans.into.name);
+		}
+		write!(f, "")
 	}
 }
 
-struct Specimen {
-	name: Option<String>,
-	species: Box<Species>,
+impl fmt::Display for Element {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		let s = match *self {
+			Element::Normal => "Normal",
+			Element::Flying => "Flying",
+			Element::Grass => "Grass",
+			Element::Bug => "Bug",
+			Element::Poison => "Poison",
+			Element::Fire => "Fire",
+			Element::Rock => "Rock",
+			Element::Ground => "Ground",
+			Element::Fighting => "Fighting",
+			Element::Water => "Water",
+			Element::Ice => "Ice",
+			Element::Electric => "Electric",
+			Element::Psychic => "Psychic",
+			Element::Ghost => "Ghost",
+			Element::Dragon => "Dragon"
+		};
+		write!(f, "{}", s)
+	}
+}
+
+impl fmt::Display for StoneType {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		let s = match *self {
+			StoneType::FireStone => "Fire Stone",
+			StoneType::WaterStone => "Water Stone",
+			StoneType::ThunderStone => "Thunder Stone",
+			StoneType::LeafStone => "Leaf Stone",
+			StoneType::MoonStone => "Moon Stone"
+		};
+		write!(f, "{}", s)
+	}
+}
+
+struct Specimen<'a> {
+	name: Option<&'static str>,
+	species: &'a Species<'a>,
 	level: u32,
-	status: Status,
+	status: Option<Status>,
 	max_hp: u32,
 	hp: u32,
 	exp: u32,
@@ -88,13 +140,123 @@ struct Specimen {
 }
 
 fn main() {
+	let tackle = Move { name: "Tackle", type1: Element::Normal };
+	let growl = Move { name: "Growl", type1: Element::Normal };
+	let leech_seed = Move { name: "Leech Seed", type1: Element::Grass };
+	let vine_whip = Move { name: "Vine Whip", type1: Element::Grass };
+	let poison_powder = Move { name: "Poison Powder", type1: Element::Poison };
+	let razor_leaf = Move { name: "Razor Leaf", type1: Element::Grass };
+	let growth = Move { name: "Growth", type1: Element::Normal };
+	let sleep_powder = Move { name: "Sleep Powder", type1: Element::Grass };
+	let solar_beam = Move { name: "Solar Beam", type1: Element::Grass };
 	let mewtwo = Species {
 		index: 150,
-		name: "Mewtwo".to_string(),
+		name: "Mewtwo",
 		type1: Element::Psychic,
 		type2: None,
 		transforms: vec![],
 		schedule: vec![]
 	};
+	let flareon = Species {
+		index: 136,
+		name: "Flareon",
+		type1: Element::Fire,
+		type2: None,
+		transforms: vec![],
+		schedule: vec![]
+	};
+	let jolteon = Species {
+		index: 135,
+		name: "Jolteon",
+		type1: Element::Electric,
+		type2: None,
+		transforms: vec![],
+		schedule: vec![]
+	};
+	let vaporeon = Species {
+		index: 134,
+		name: "Vaporeon",
+		type1: Element::Water,
+		type2: None,
+		transforms: vec![],
+		schedule: vec![]
+	};
+	let eevee = Species {
+		index: 133,
+		name: "Eevee",
+		type1: Element::Normal,
+		type2: None,
+		transforms: vec![
+			Transform { mode: TransformMode::ByStone(StoneType::FireStone), into: &flareon },
+			Transform { mode: TransformMode::ByStone(StoneType::ThunderStone), into: &jolteon },
+			Transform { mode: TransformMode::ByStone(StoneType::WaterStone), into: &vaporeon }
+		],
+		schedule: vec![]
+	};
+	let venusaur = Species {
+		index: 3,
+		name: "Venusaur",
+		type1: Element::Grass,
+		type2: Some(Element::Poison),
+		transforms: vec![],
+		schedule: vec![]
+	};
+	let ivysaur = Species {
+		index: 2,
+		name: "Ivysaur",
+		type1: Element::Grass,
+		type2: Some(Element::Poison),
+		transforms: vec![
+			Transform { mode: TransformMode::ByLevel(32), into: &venusaur }
+		],
+		schedule: vec![]
+	};
+	let bulbasaur = Species {
+		index: 1,
+		name: "Bulbasaur",
+		type1: Element::Grass,
+		type2: Some(Element::Poison),
+		transforms: vec![
+			Transform { mode: TransformMode::ByLevel(16), into: &ivysaur }
+		],
+		schedule: vec![
+			(1, tackle),
+			(1, growl),
+			(7, leech_seed),
+			(13, vine_whip),
+			(20, poison_powder),
+			(27, razor_leaf),
+			(34, growth),
+			(41, sleep_powder),
+			(48, solar_beam)
+		]
+	};
+	let vileplume = Species {
+		index: 45,
+		name: "Vileplume",
+		type1: Element::Grass,
+		type2: Some(Element::Poison),
+		transforms: vec![],
+		schedule: vec![]
+	};
+	let nuptup = Specimen {
+		name: Some("Nuptup"),
+		species: &vileplume,
+		level: 35,
+		status: None,
+		max_hp: 100,
+		hp: 80,
+		exp: 365,
+		defense: 50,
+		attack: 30
+	};
+	println!("{}", bulbasaur);
+	println!("{}", ivysaur);
+	println!("{}", venusaur);
+	println!("{}", vileplume);
+	println!("{}", eevee);
+	println!("{}", vaporeon);
+	println!("{}", jolteon);
+	println!("{}", flareon);
 	println!("{}", mewtwo);
 }
